b0ec507162460085e16e7f08f3ac2d9e83ec43be
Jukexx <imJukexx@gmail.com>
2020-04-18 01:42:38 +0100

    Added API to verify the zipcode Cleaned up the code Added clean previous visual response function Created visual response to user

--- server.js
+++ server.js
@@ -1,62 +1,72 @@
-//
-
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
 const fetch = require("node-fetch");
+
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
 var bodyParser = require('body-parser');
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
-
 const port = 8000;
-let apiKey = '57dab2d04f62a1543169c35a20f27a8d';
+const apiKey = '57dab2d04f62a1543169c35a20f27a8d';
  
-//  function getBaseUrl(zipCode, countryCode){
-//      console.log('getBaseUrl Serverside is working');
-//     let baseURL = `api.openweathermap.org/data/2.5/weather?zip=${zipCode},${countryCode}&appid=${apiKey}`;
-//     return baseURL;
-// }
-
 // Setup Server
-//api.openweathermap.org/all?zip=${zipCode},${countryCode}&appid=${apiKey}
-app.get('/all', sendData);
+app.get('/all', getWeatherForPostCode);
 
-function sendData (req, res) {
-    //let url = getBaseUrl(req.zipCode, 'us');
+function getWeatherForPostCode (req, res) {
+
     let countryCode = 'us';
-    let endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${req.query.zipCode},${countryCode}&appid=${apiKey}`;
-        
+    let endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${req.query.zipCode},${countryCode}&appid=${apiKey}&units=metric`;
+
     const retrieveData = async (endpoint) =>{ 
         const request = await fetch(endpoint);
+
         try {
-        // Transform into JSON
-        const allData = await request.json();
-        res.send(allData);
+            // Transform into JSON
+            const response = await request.json();
+            res.status(200).send(response);
         }
         catch(error) {
             console.log("error", error);
             // appropriately handle the error
-        }
-            
-        };        
-        
+        }       
+    };        
         retrieveData(endpoint);
-}
-
+};
 
   
+app.get('/checkzipcode', checkZipCode);
+function checkZipCode (req, res) {
 
+    let endpoint = `https://api.bring.com/pickuppoint/api/postalCode/us/getCityAndType/${req.query.zipCode}.json`;
+        
+    const retrieveData = async (endpoint) =>{ 
+        const request = await fetch(endpoint);
+        const allData = await request.json();
+
+        if(request.status !== 200){
+            res.status(400).send(allData);
+        }
+        else{
+            res.status(200).send(allData);
+        }
+    };  
+        retrieveData(endpoint);
+};
 
 
 const server = app.listen(port,listening);
@@ -64,7 +74,7 @@ const server = app.listen(port,listening);
 function listening(){
     console.log('Server running');
     console.log(`Running on localhost:${port}`);
-}
+};
 
 //POST route
 
--- website/app.js
+++ website/app.js
@@ -1,48 +1,138 @@
 /* Global Variables */
 
-//import { cachedDataVersionTag } from "v8";
+function initializePage() {
+    document.querySelector('#generate').addEventListener('click', getWeatherForZipCode);
+};
 
-// function getBaseUrl(zipCode, countryCode){
-//     let baseURL = `127.0.0.1:8000/all?zipCode=${zipCode}`;
-//     return baseURL;
-// }
+function getWeatherForZipCode() {
 
-
-function initializePage(){
-    document.querySelector('#generate').addEventListener('click', getWeatherForPostCode);
-
-}
-
-function getWeatherForPostCode(){
+    clearCurrentWeatherInfo();
     let zipCode = getZipCode();
-    let url = getUrl(zipCode);
-    getWeather(url);
-}
+    let isFeelingPopulated = checkFeeling();
 
-const getWeather = async (url='') =>{ 
+    if (!zipCode || !checkFeeling()) {
+        return;
+    }
+
+    let zipCodeUrl = getZipCodeUrl(zipCode);
+
+    if (!validateZipCode(zipCodeUrl)) {
+        return;
+        //TODO set zipCode textbox background to red
+    }
+
+    let weatherUrl = getWeatherUrl(zipCode);
+    getWeather(weatherUrl).json;
+};
+
+//#region server calls
+
+const validateZipCode = async (zipCodeUrl = '') => {
+    const request = await fetch(zipCodeUrl);
+    const isValidUrl = await request.json();
+
+    if (request.status === 400) {
+        console.log('invalid zipcode');
+        setFieldClassToError('zip');
+        document.querySelector(`#invalidzipwarning`).style.display = 'block'; 
+    }
+    else {
+        console.log('valid zipcode');
+        console.log('city ', isValidUrl.postalCode.city);
+        setFieldClassToValid('zip');
+    }
+};
+
+const getWeather = async (url = '') => {
     const request = await fetch(url);
     try {
         const allData = await request.json();
-        console.log('alldata:'+ JSON.stringify(allData, null, 4));
+        //Writes in the HTML the location, sky and temperature of the zip code added
+        document.querySelector('#cityweather').innerHTML = `Weather in : ${allData.sys.country}, ${allData.name}`;
+        document.querySelector('#currentweather').innerHTML = `The sky is ${allData.weather[0].main} and the temperature is : ${Math.round(allData.main.temp)}°C`;
+        document.querySelector('#zip').style.backgroundColor = "green";
     }
-    catch(error) {
-      console.log("error", error);
-      // appropriately handle the error
-    }
-  };
+    catch (error) {
 
-  
-// Create a new date instance dynamically with JS
-// let d = new Date();
-// let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+        if (TypeError == 'SpecificError') {
+            document.querySelector('#zip').style.backgroundColor = "red";
+        } 
+        else {
+            document.querySelector('#zip').style.backgroundColor = "red";
+            console.log("error", error);
+        }
+    }
+};
+
+
+//#endregion
+
+//#region helper methods
+function getWeatherUrl(zipCode) {
+    let url = `/all?zipCode=${zipCode}`;
+    return url;
+};
+
+function getZipCodeUrl(zipCode) {
+    return `/checkzipcode?zipCode=${zipCode}`;
+};
+
+function checkZipCode() {
+    let zipCodeField = document.querySelector('#zip').value;
+};
+
+function clearCurrentWeatherInfo(){
+    //write this
+    document.querySelector('#cityweather').innerHTML = ``;
+    document.querySelector('#currentweather').innerHTML = ``;
+    document.querySelector(`#invalidzipwarning`).style.display = 'none'; 
+};
+
+//#endregion
+
+//#region UI Manipulation
+function checkFeeling() {
+    let feelingsField = document.querySelector('#feelings').value;
+
+    if (feelingsField === '') {
+        setFieldClassToError('feelings')
+        return false;
+    }
+    else {
+        setFieldClassToValid('feelings');
+        return true;
+    }
+};
+
+function setFieldClassToError(fieldName) {
+    //Changes class of invalid input
+    document.querySelector(`#${fieldName}`).setAttribute('class', 'error');
+};
+
+function setFieldClassToValid(fieldName) {
+    //Changes class of valid input
+    document.querySelector(`#${fieldName}`).setAttribute('class', 'valid'); 
+};
+
+function getZipCode() {
+
+    let zipCode = document.querySelector('#zip').value;
+
+    if (zipCode === '') {
+        return false;
+    }
+    else {
+        return document.querySelector('#zip').value;
+    }
+};
+//#endregion
 
 initializePage();
 
-function getUrl(zipCode) {
-    let url = `/all?zipCode=${zipCode}`;
-    return url;
-}
 
-function getZipCode() {
-    return document.querySelector('#zip').value;
-}
+
+
+
+
+
+
